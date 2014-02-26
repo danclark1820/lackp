dd09df01032f5403151de7a9f39a065401b42c5d
danclark1820 <danclark1820@gmail.com>
2014-02-26 17:46:12 -0500

    Made LACKP player and positon pages dynamic to one page

--- lackp_server.rb
+++ lackp_server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'CSV'
+require 'pry'
 
 def csv_to_hash(file_name)
   lackp = {}
@@ -16,37 +17,108 @@ def csv_to_hash(file_name)
   lackp
 end
 
-
-
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
   @lackp = csv_to_hash('lackp_starting_rosters.csv')
   @teams = []
+  @positions = []
   @lackp.each do |key, value|
     if !(@teams.include?(value[:team]))
-      @teams<<value[:team]
+      @teams << value[:team]
     end
   end
+  @teams.each do |x|
+    x.downcase!.gsub!(' ', '_')
+  end
+
+  @lackp.each do |key, value|
+    if !(@positions.include?(value[:position]))
+      @positions << value[:position]
+    end
+  end
+
+  @positions.each do |x|
+    x.downcase!.gsub!(' ', '_')
+  end
+
   erb :index
 end
 
 get '/simpson_slammers' do
   @lackp = csv_to_hash('lackp_starting_rosters.csv')
-  erb :simpson_slammers
+  @key = "Simpson Slammers"
+  erb :teams
 end
 
 get '/jetson_jets' do
   @lackp = csv_to_hash('lackp_starting_rosters.csv')
-  erb :jetson_jets
+  @key = "Jetson Jets"
+  erb :teams
 end
 
 get '/flinestone_fire' do
   @lackp = csv_to_hash('lackp_starting_rosters.csv')
-  erb :flinestone_fire
+  @key = "Flinestone Fire"
+  erb :teams
 end
 
 get '/griffin_goats' do
   @lackp = csv_to_hash('lackp_starting_rosters.csv')
-  erb :griffin_goats
+  @key = "Griffin Goats"
+  erb :teams
+end
+
+get '/catcher' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Catcher"
+  erb :position
+end
+
+get '/pitcher' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Pitcher"
+  erb :position
+end
+
+get '/1st_base' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "1st Base"
+  erb :position
+end
+
+get '/2nd_base' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "2nd Base"
+  erb :position
+end
+
+get '/3rd_base' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "3rd Base"
+  erb :position
+end
+
+get '/shortstop' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Shortstop"
+  erb :position
+end
+
+get '/right_field' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Right Field"
+  erb :position
+end
+
+get '/center_field' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Center Field"
+  erb :position
+end
+
+get '/left_field' do
+  @lackp = csv_to_hash('lackp_starting_rosters.csv')
+  @key = "Left Field"
+  erb :position
 end
